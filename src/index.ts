36a7f13922e3a5879f4651739002714f2032ad34
Hector Abreu <alu0101068855@ull.edu.es>
2022-04-03 19:18:42 +0000

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export function add (n1: number, n2: number): number{
-  return n1 + n2;
+/**
+ * Adds two numbers
+ * @param firstNumber Consists of the first numeric operand of the addition
+ * @param secondNumber Consists of the second numeric operand of the addition
+ * @return The addition of the two numbers `firstNumber` and `secondNumber`
+ *
+ * Usage:
+ * ```typescript
+ * add(1, 7) = 8
+ * add(1.7, 3.5) = 5.2
+ * ```
+ */
+ export function add(firstNumber: number, secondNumber: number) {
+  return firstNumber + secondNumber;
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,9 +1,17 @@
 import 'mocha';
-import { expect } from 'chai';
-import { add } from '../src/index';
+import {expect} from 'chai';
+import {add} from '../src/index';
 
-describe('Test: ', () => {
-  it('Index', () => {
-    expect(add(1,2)).to.eql(3);
+describe('add function tests', () => {
+  it('add(1, 8) returns value 9', () => {
+    expect(add(1, 8)).to.be.equal(9);
   });
-}); 
+
+  it('add(-1, 8) returns value 7', () => {
+    expect(add(-1, 8)).to.be.equal(7);
+  });
+
+  it('add(1.2, 3.5) returns value 4.7', () => {
+    expect(add(1.2, 3.5)).to.be.equal(4.7);
+  });
+});
